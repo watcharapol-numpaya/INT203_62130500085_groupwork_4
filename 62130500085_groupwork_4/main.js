6aa64ed9268efada39a5e42a7126bb0c9c07e59c
watcharapol <watcharapol.num@mail.kmutt.ac.th>
2021-02-21 23:40:02 +0700

    fix like button

--- 62130500085_groupwork_4/main.js
+++ 62130500085_groupwork_4/main.js
@@ -10,34 +10,26 @@ const app = {
             tasks: [{
                     title: 'Shiba inu',
                     src: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/26114711/Shiba-Inu-standing-in-profile-outdoors.jpg',
-                    detail: 'The Shiba Inu dog breed was originally bred to flush birds and small game, and was occasionally used to hunt wild boar. They’re one of Japan’s six native breeds: Akita (large), Kishu, Hokkaido, Kai, Shikoku (medium), and Shiba (small).',
-
-                    lovebor: true,
-                    lovebold: false
+                    detail: 'The Shiba Inu dog breed was originally bred to flush birds and small game, and was occasionally used to hunt wild boar. They’re one of Japan’s six native breeds: Akita (large), Kishu, Hokkaido, Kai, Shikoku (medium), and Shiba (small).',                   
+                    like: false
                 },
                 {
                     title: 'Golden',
                     src: 'https://i.pinimg.com/originals/44/52/eb/4452eb2986b307f62b82d622f21ea90e.jpg',
                     detail: 'The Golden Retriever is one of the most popular dog breeds in the United States. The breed’s friendly, tolerant attitude makes them great family pets, and their intelligence makes them highly capable working dogs. ',
-
-                    lovebor: true,
-                    lovebold: false
+                    like: false
                 },
                 {
                     title: 'Pug',
                     src: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/Mops_oct09_cropped2.jpg',
                     detail: 'The pug is a breed of dog with physically distinctive features of a wrinkly, short-muzzled face, and curled tail. The breed has a fine, glossy coat that comes in a variety of colours, most often light brown (fawn) or black, and a compact, square body with well-developed muscles. ',
-
-                    lovebor: true,
-                    lovebold: false
+                    like: false
                 },
                 {
                     title: 'chiwawa',
                     src: 'https://i.pinimg.com/originals/7b/7b/b8/7b7bb80169c31df6b4251aa967758020.jpg',
                     detail: 'The Chihuahua is a balanced, graceful dog of terrier-like demeanor, weighing no more than 6 pounds. The rounded “apple” head is a breed hallmark. The erect ears and full, luminous eyes are acutely expressive. Coats come in many colors and patterns, and can be long or short.',
-
-                    lovebor: true,
-                    lovebold: false
+                    like: false
                 }
             ]
         }
@@ -49,8 +41,8 @@ const app = {
             this.query[index].hide = !this.query[index].hide
         },
         toggleLike(index) {
-            this.query[index].lovebold = !this.query[index].lovebold
-            this.query[index].lovebor = !this.query[index].lovebor
+            this.query[index].like = !this.query[index].like
+           
 
 
 
@@ -72,7 +64,7 @@ const app = {
     },
     computed: {
         countLove() {
-            return this.tasks.filter(t => t.lovebold).length
+            return this.tasks.filter(t => t.like).length
         },
         query() {
             return this.tasks.filter(q => {
